c4273f115fdaa624ed1e3a1c5442da194cf47d47
Alberto Hormazabal <ahormazabal@exatech.cl>
2016-05-23 18:41:21 -0400

    adds more complex startup script to sync on all instances added party hard

--- dockers/rundeck/scripts/run.sh
+++ dockers/rundeck/scripts/run.sh
@@ -3,7 +3,8 @@
 # Configure general stuff.
 ./rdpro-installer configure-server-hostname --server-hostname $RUNDECK_NODE --rdeck-base $HOME
 ./rdpro-installer configure-server-name --rdeck-base $HOME --server-name $RUNDECK_NODE
-./rdpro-installer configure-server-url --server-url $RUNDECK_URL --rdeck-base $HOME
+./rdpro-installer configure-server-url --server-url "" --rdeck-base $HOME
+#./rdpro-installer configure-server-url --server-url $RUNDECK_URL --rdeck-base $HOME
 
 
 # Configure passive mode if applies.
@@ -15,9 +16,61 @@ then
 else
     echo "This is the active Instance";
     # Here all instructions to run in the active.
+
+    # Configure JobReplication Plugin
+    # Configure Execution Replication Plugin
+
+fi
+
+# PARTY HARD
+./rdpro-installer start --rdeck-base $HOME
+
+
+# Wait for server to start
+LOGFILE=$HOME/server/logs/catalina.out
+SUCCESS_MSG="INFO: Server startup in"
+MAX_ATTEMPTS=18
+SLEEP=10
+echo "waiting for $RUNDECK_NODE to start"
+declare -i count=0
+while (( count <= MAX_ATTEMPTS ))
+do
+    if ! grep "${SUCCESS_MSG}" "$LOGFILE"
+    then  echo -n "."; # output a progress character.
+    else  break; # found successful startup message.
+    fi
+    (( count += 1 ))  ; # increment attempts counter.
+    (( count == MAX_ATTEMPTS )) && {
+        echo >&2 "FAIL: Reached max attempts to find success message in catalina.out. Exiting."
+        exit 1
+    }
+    sleep $SLEEP; # wait before trying again.
+done
+echo "RUNDECK NODE $RUNDECK_NODE started successfully!!"
+
+
+### POST CONFIG
+
+# Create Replication Jobs and API Tokens.
+if [[ X$RUNDECK_ROLE == 'Xpassive' ]] ;
+then
+    echo "Post configuring passive instance";
+    # Here all instructions to run in the passive
+
+else
+    echo "Post configuring active instance";
+    # Here all instructions to run in the active.
+
+    # Request apitoken from server 2
+    # Configure JobReplication Plugin
+    # Configure Execution Replication Plugin
+
 fi
 
 
-# party: on
-./rdpro-installer start --rdeck-base $HOME && tail -F $HOME/server/logs/catalina.out
+### Signal READY
+# here we should leave some file in a shared folder to signal that the server is ready. so tests can begin.
 
+
+# Keep alive
+tail -F $HOME/server/logs/catalina.out
