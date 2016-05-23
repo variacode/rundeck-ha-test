7a6d943eb77cbe671255840fc583699fbafe720c
Alberto Hormazabal <ahormazabal@exatech.cl>
2016-05-23 18:43:37 -0400

    adds some instructions

--- dockers/rundeck/scripts/run.sh
+++ dockers/rundeck/scripts/run.sh
@@ -12,14 +12,10 @@ if [[ X$RUNDECK_ROLE == 'Xpassive' ]] ;
 then
     echo "This is the passive instance. Configuring passive Node";
     ./rdpro-installer configure-execution-mode --rdeck-base $HOME --execution-mode passive;
-    # Here all instructions to run in the passive
+    # Here all instructions to run in the passive pre-start
 else
     echo "This is the active Instance";
-    # Here all instructions to run in the active.
-
-    # Configure JobReplication Plugin
-    # Configure Execution Replication Plugin
-
+    # Here all instructions to run in the active pre-start
 fi
 
 # PARTY HARD
@@ -55,22 +51,21 @@ echo "RUNDECK NODE $RUNDECK_NODE started successfully!!"
 if [[ X$RUNDECK_ROLE == 'Xpassive' ]] ;
 then
     echo "Post configuring passive instance";
-    # Here all instructions to run in the passive
+    # Here all instructions to run in the passive post-start
 
 else
     echo "Post configuring active instance";
-    # Here all instructions to run in the active.
+    # Here all instructions to run in the active post-start
 
-    # Request apitoken from server 2
+    # Request apitoken from server 2 (use rrtokens)
     # Configure JobReplication Plugin
     # Configure Execution Replication Plugin
 
 fi
 
-
 ### Signal READY
 # here we should leave some file in a shared folder to signal that the server is ready. so tests can begin.
-
+# touch some/dir/$RUNDECK_NODE.ready
 
 # Keep alive
 tail -F $HOME/server/logs/catalina.out
