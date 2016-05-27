6db10c3d2831d719bc67598ea826b9de7107a686
Alberto Hormazabal <ahormazabal@exatech.cl>
2016-05-27 08:50:47 -0400

    some tuning

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,7 +6,7 @@ set -e
 echo "Beginning tests"
 
 #Run stage 1
-docker-compose exec --user rundeck testnode ./scripts/run-step1.sh
+docker-compose exec -T --user rundeck testnode ./run-step1.sh
 sleep 3;
 
 # Kill main rundeck instance.
@@ -16,11 +16,11 @@ sleep 3;
 
 # Trigger active mode on passive node.
 echo "Setting node2 to active mode."
-docker-compose exec --user rundeck rundeck2 ./rrsystem rundeck-system:set-execution-mode --execution-mode active --url http://rundeck2:4440/rundeckpro-dr --username admin --password admin
+docker-compose exec -T --user rundeck rundeck2 ./rrsystem rundeck-system:set-execution-mode --execution-mode active --url http://rundeck2:4440/rundeckpro-dr --username admin --password admin
 sleep 3;
 
 #Run stage 2
-docker-compose exec --user rundeck testnode ./run-step2.sh
+docker-compose exec -T --user rundeck testnode ./run-step2.sh
 sleep 3;
 
 
