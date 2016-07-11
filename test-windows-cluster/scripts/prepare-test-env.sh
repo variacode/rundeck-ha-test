a62f8c0b692e5ce837b49c7752c5b050232eb3e6
Alberto Hormazabal <ahormazabal@exatech.cl>
2016-07-11 09:34:48 -0400

    resolves provision of software and artifacts

--- test-windows-cluster/scripts/prepare-test-env.sh
+++ test-windows-cluster/scripts/prepare-test-env.sh
@@ -2,9 +2,42 @@
 
 set -ex
 
+#Read config
+. metadata
+
 #Create temp dir
 WORKDIR=tmp
+FILESDIR=files
 mkdir -p $WORKDIR
+
+### Get software installers
+# 7z
+if [ ! -f $FILESDIR/$ZIP7_INSTALLER ]
+then
+  echo "Downloading 7ZIP"
+  wget -P $FILESDIR $SOFTWARE_URL/$ZIP7_INSTALLER
+fi
+
+# JRE
+if [ ! -f $FILESDIR/$JRE_INSTALLER ]
+then
+  echo "Downloading JRE Installer"
+  wget -P $FILESDIR $SOFTWARE_URL/$JRE_INSTALLER
+fi
+
+# Tomcat
+if [ ! -f $FILESDIR/$TOMCAT_INSTALLER ]
+then
+  echo "Downloading Tomcat Installer"
+  wget -P $FILESDIR $SOFTWARE_URL/$TOMCAT_INSTALLER
+fi
+
+# RUNDECK
+echo "Downloading Rundeck WAR"
+wget -O $FILESDIR/rundeckpro.war $ARTIFACTS_URL/$RUNDECK_WAR
+
+exit 0
+
 
 # Get rome rundeck tools
 git clone https://github.com/rerun/rerun $WORKDIR/rerun
